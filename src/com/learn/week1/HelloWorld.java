44715af8a55b82948ca68136896a3463eb18571e
ethanhwang <1055256192@qq.com>
2025-04-25 20:28:27 +0800

    Initial commit: Add HelloWorld program and .gitignore

---
+++ src/com/learn/week1/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.learn.week1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My first Java program in IntelliJ IDEA");
+        String learningStep = "Running the program";
+        System.out.println("Current step:" + learningStep);
+
+        int day = 3;
+        System.out.println("Today is day " + day + " of the week");
+    }
+}
